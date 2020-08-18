b521b6cbab22b6ea585c27444712365dff286909
Marlon Roches <roches856@gmail.com>
2020-08-18 15:10:31 -0600

    Inicio de serializacion

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/Data.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/Data.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Proyecto_Compiladores_2020.Data
 {
     class Data
     {
+        private static Data _instance = null;
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new Data();
+                return _instance;
+            }
+        }
+        private static Dictionary<string, string> Reservadas_Sustitucion = new Dictionary<string, string>();
+        //void int double boolean string class const interface null this extends implements for while if else return break New System out printl
+        private static Dictionary<string, string> Variables_Sustitucion = new Dictionary<string, string>();
+        
+        public void Sustituir(string code)
+        {
+
+            var file= new StreamReader("Reservadas.txt");
+            Reservadas_Sustitucion = JsonConvert.DeserializeObject<Dictionary<string, string>>(file.ReadToEnd());
+
+            var CodeArray = code.Split();
+
+            foreach (var item in CodeArray)
+            {
+                if (Reservadas_Sustitucion.ContainsKey(item))
+                {
+
+                }
+            }
+        }
+
     }
 }
--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Program.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Proyecto_Compiladores_2020.Data;
+using Newtonsoft.Json;
 
 namespace Proyecto_Compiladores_2020
 {
@@ -11,8 +12,35 @@ namespace Proyecto_Compiladores_2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Linea De Codigo");
-            Data.Data.Instance.Sustituir(Console.ReadLine());
+            Console.WriteLine("Linea De Codigo Java");
+            //Data.Data.Instance.Sustituir(Console.ReadLine());
+
+            Dictionary<string, string> ldiccionariol = new Dictionary<string, string>();
+
+            ldiccionariol.Add("void", "◙");
+            ldiccionariol.Add("int","♂");
+            ldiccionariol.Add("double","♀");
+            ldiccionariol.Add("boolean","♪");
+            ldiccionariol.Add("string", "♫");
+            ldiccionariol.Add("class","☼");
+            ldiccionariol.Add("const","►");
+            ldiccionariol.Add("interface","◄");
+            ldiccionariol.Add("null","↕");
+            ldiccionariol.Add("this","‼");
+            ldiccionariol.Add("extends","¶");
+            ldiccionariol.Add("implements","§");
+            ldiccionariol.Add("for","▬");
+            ldiccionariol.Add("while","↨");
+            ldiccionariol.Add("if","↑");
+            ldiccionariol.Add("else","↓");
+            ldiccionariol.Add("return","→");
+            ldiccionariol.Add("break","←");
+            ldiccionariol.Add("New","∟");
+            ldiccionariol.Add("System","↔");
+            ldiccionariol.Add("out","▲");
+            ldiccionariol.Add("println","▼");
+
+            var json5 = JsonConvert.SerializeObject(ldiccionariol);
         }
     }
 }
