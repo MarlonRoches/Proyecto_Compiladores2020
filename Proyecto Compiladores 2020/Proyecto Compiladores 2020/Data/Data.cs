17d851790aa17ffd12a86d556f6088bdfae1e38b
Alexander Villatoro <alex.gabrielvillatoro@gmail.com>
2020-08-22 16:49:21 -0600

    Update Data Class

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/Data.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/Data.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace Proyecto_Compiladores_2020.Data
 {
@@ -27,14 +28,14 @@ namespace Proyecto_Compiladores_2020.Data
         public void Sustituir(string code)
         {
             var asds = "";
+
             Regex Keywords = new Regex(@"^(void|int|double|boolean|string|class|const|interface|null|this|extends|implements|for|while|if|else|return|break|New|System|out|println)$");
             Regex Operadores = new Regex(@"^(\+|-|\*|/|%|<|<=|>|>=|=|==|!=|&&|\|\||!|;|,|\.|\[|\]|\(|\)|{|}|\[\]|\(\)|{})$");
             Regex Boolean = new Regex("true|false");
             Regex Digit = new Regex(@"^([0-9]+)*$");
-            Regex Hexadecimal = new Regex(@"^([0X|0x])([0-9A-Fa-f]+)$");
+            Regex Hexadecimal = new Regex(@"^(0[xX]).([0-9a-fA-F]+)$");
             Regex Double = new Regex(@"^(([0-9]+.[0-9]+|.[0-9]+)|([0-9]+.(E|e)(-|\+)?[0-9]+))$");
             Regex Identifier = new Regex(@"^([A-Za-z]+[0-9]*)$");
-
 
             var file = new StreamReader("Reservadas.txt");
             Reservadas_Sustitucion = JsonConvert.DeserializeObject<Dictionary<string, string>>(file.ReadToEnd());
@@ -42,11 +43,18 @@ namespace Proyecto_Compiladores_2020.Data
 
             var fileCode = new StreamReader("TestCode.txt");
             var rawCode = fileCode.ReadToEnd();
-            fileCode.Close();
 
+            fileCode.Close();
             foreach (var item in Reservadas_Sustitucion.Keys)
             {
-                rawCode = rawCode.Replace($"{item}", $" {item} ");
+                if (item == "int")
+                {
+                    rawCode = rawCode.Replace(item, "int");
+                }
+                else
+                {
+                    rawCode = rawCode.Replace($"{item}", $" {item} ");
+                }
             }
             rawCode = rawCode.Replace("\r\n", " ").Replace("\t", " ").Replace(";", " ; ");
             var espacios = "  ";
@@ -63,12 +71,11 @@ namespace Proyecto_Compiladores_2020.Data
                 rawCode = rawCode.Replace(espacios, " ");
                 espacios += " ";
             }
-            /**/
+            
             var CodeArray = rawCode.Split();
             foreach (var tokenActual in CodeArray)
             {
-                if (Reservadas_Sustitucion.ContainsKey(tokenActual))
-                {
+               
                     if (Keywords.IsMatch(tokenActual))
                     {
                         Console.WriteLine("Es una palabra reservada: " + tokenActual);
@@ -103,23 +110,19 @@ namespace Proyecto_Compiladores_2020.Data
                     }
 
                     //Console.WriteLine($"Token {tokenActual}: T_{tokenActual} -> {Reservadas_Sustitucion[tokenActual]}");
-
-                }
-                else
-                {
-                        Console.WriteLine("No es un token");
-                    //puede ser un id
-                    //tabla de ids validarsintaxis_Id(tokenActual)
-                    //puede tener error
-
-                }
-                /*if (Reservadas_Sustitucion.ContainsKey(item))
                 
+                //else
+                //{
+                //        Console.WriteLine("No es un token");
+                //    //puede ser un id
+                //    //tabla de ids validarsintaxis_Id(tokenActual)
+                //    //puede tener error
 
+                //}
+                /*if (Reservadas_Sustitucion.ContainsKey(item))
+               
                 }*/
             }
-            
-            
             var linea = Console.ReadLine();
         }
 
