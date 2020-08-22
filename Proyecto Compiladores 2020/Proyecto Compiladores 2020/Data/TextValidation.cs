df982d4572c2a7ae98485fa07610d1d11e379dcf
MarlonRoches <roches856@gmail.com>
2020-08-22 14:19:59 -0600

    new Class

---
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/TextValidation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Compiladores_2020.Data
+{
+    class TextValidation
+    {
+        private static TextValidation _instance = null;
+        public static TextValidation Instance
+        {
+            get
+            {
+                if (_instance == null) _instance = new TextValidation();
+                return _instance;
+            }
+        }
+
+
+    }
+}
