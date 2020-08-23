0035c8c2a9dd55989d1ffec443dcd2b260183927
MarlonRoches <roches856@gmail.com>
2020-08-23 17:49:04 -0600

    Multiline comentary

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/TextValidation.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/TextValidation.cs
@@ -48,20 +48,44 @@ namespace Proyecto_Compiladores_2020.Data
                 else if (linea.Contains("/*"))
                 {
                     //inicio de comentario multilinea
+                    var actual = linea;
                     while (!linea.Contains("*/"))
                     {
-                        linea += $"{fileCode.ReadLine()}\n";
+                        linea += $"{fileCode.ReadLine()}";
+
+                        if (linea.Contains("/*"))
+                        {
+                            //contiene inicio de comentario
+                            //code ........... /*
+                            var aux = linea.Remove(0, linea.IndexOf("/*"));
+                            linea = linea.Replace(aux, $"æ{Comentarios_Index}\n");
+                            AgregarComentario(aux);
+                        }
+                        else if (linea.Contains("*/"))
+                        {
+                            //contiene final de comentario
+                            //*/  code......
+                            var aux = linea.Remove(linea.IndexOf("*/"), linea.Length - 1);
+
+                        }
+                        else
+                        {
+                            //Medio comentarios
+                            //agarrar todos los del medio
+
+                        }
                         if (linea == null)
                         {
                             //error de comentario no cerrado
+
                             break;
                         }
                     }
-                    var comentarioMultilinea = linea.Remove(0, linea.IndexOf("/*"));
-                    comentarioMultilinea = comentarioMultilinea.Substring(0, comentarioMultilinea.IndexOf("*/") + 2);
-                    Comentarios.Add($"æ{Comentarios_Index}æ", comentarioMultilinea);
-                    linea = linea.Replace(comentarioMultilinea, $"æ{Comentarios_Index}");
-                    Comentarios_Index++;
+                    //var comentarioMultilinea = linea.Remove(0, linea.IndexOf("/*"));
+                    //comentarioMultilinea = comentarioMultilinea.Substring(0, comentarioMultilinea.IndexOf("*/") + 2);
+                    //Comentarios.Add($"æ{Comentarios_Index}", comentarioMultilinea);
+                    //linea = linea.Replace(comentarioMultilinea, $"æ{Comentarios_Index}");
+                    //Comentarios_Index++;
                     while (linea.Contains("//"))
                     {
                         linea = parseComentarioUnilinea(linea);
@@ -100,9 +124,10 @@ namespace Proyecto_Compiladores_2020.Data
             return linea;
         }
 
-        void AgregarComentario(string linea)
+        void AgregarComentario(string comentario)
         {
-
+            Comentarios.Add($"æ{Comentarios_Index}", comentario);
+            Comentarios_Index++;
         }
         string CleanCode(string rawCode)
         {
@@ -123,5 +148,21 @@ namespace Proyecto_Compiladores_2020.Data
             return rawCode;
         }
 
+        public void Error()
+        {
+            StreamReader streamReader = new StreamReader("TestCode.txt");
+            int line = 0;
+            int col = 0;
+            while (!streamReader.EndOfStream)
+            {
+                string readline = streamReader.ReadLine();
+                line++;
+                string[] validate = readline.Split();
+
+                //si se hace un substring se le puede hacer un col++  para poder saber la columna y su error
+
+            }
+
+        }
     }
 }
