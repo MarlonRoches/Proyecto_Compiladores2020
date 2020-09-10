ad293acbd4d038d5f2ce313d6495cc62efeede1a
MarlonRoches <roches856@gmail.com>
2020-09-10 14:26:08 -0600

    Match token

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/GrammarValidation.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/GrammarValidation.cs
@@ -18,7 +18,11 @@ namespace Proyecto_Compiladores_2020.Data
                 return _instance;
             }
         }
-        private static List<string> outPutTokens = new List<string>();
+        private static List<KeyValuePair<string,string>> outPutTokens = new List<KeyValuePair<string, string>>();
+        private static List<string> sintaxError = new List<string>();
+        
+        private static int contador = 0;
+        private static string lookahead="";
         public void Lab1()
         {
             for (int i = 0; i < outPutTokens.Count; i++)
@@ -27,7 +31,34 @@ namespace Proyecto_Compiladores_2020.Data
             }
         }
 
+        private void MatchToken(string expectedToken)
+        {
+            try
+            {
+                if (lookahead == expectedToken)
+                {
+                    contador++;
+                    lookahead = outPutTokens[contador].Item1;
+                }
+                else
+                {
+                    if (lookahead == "$")
+                    {
+                        errores.Add($"Error sintactico: se esperaba ' {expectedToken} ' y ya no se tenian tokens.");
+                    }
+                    else
+                    {
+                        errores.Add($"Error sintactico: se esperaba ' {expectedToken} ' y se tenia ' {lookahead} '. {ObtenerUbicacion(tokens[contador == 0 ? contador : contador - 1].Item2)}");
+                    }
 
+                }
+            }
+            catch (Exception)
+            {
+                // se terminaron los tokens a leer 
+                throw;
+            }
+        }
 
 
 
