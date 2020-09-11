c8cf548ee595f04ffb57e91b344f44ef16d0b53d
Alexander Villatoro <alex.gabrielvillatoro@gmail.com>
2020-09-11 01:15:31 -0600

    Grammatic Almost Finished

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/GrammarValidation.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/GrammarValidation.cs
@@ -444,7 +444,7 @@ namespace Proyecto_Compiladores_2020.Data
         }
         private bool C_Prime()
         {
-            //C'				-> == D C' | != D C' | ϵ
+            //C'-> == D C' | != D C' | ϵ
             if (Actual_LookAhead == "==")
             {
                 MatchToken("==");
@@ -476,47 +476,294 @@ namespace Proyecto_Compiladores_2020.Data
         }
         private bool D()
         {
-            return false;
+            if (E())
+            {
+                return D_Prime();
+            }
+            else
+            {
+                return false;
+
+            }
         }
         private bool D_Prime()
         {
-            return false;
+            //D'-> < E D' | <= E D' | > E D' | >= E D' | ϵ  
+            if (Actual_LookAhead == "<")
+            {
+                MatchToken("<");
+                if (E())
+                {
+                    return D_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == "<=")
+            {
+                MatchToken("<=");
+                if (E())
+                {
+                    return D_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == ">")
+            {
+                MatchToken(">");
+                if (E())
+                {
+                    return D_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == ">=")
+            {
+                MatchToken(">=");
+                if (E())
+                {
+                    return D_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
         }
         private bool E()
         {
-            return false;
+            if (F())
+            {
+                return E_Prime();
+            }
+            else
+            {
+                return false;
+
+            }
         }
         private bool E_Prime()
         {
-            return false;
+            //E'-> +F E' | -F E' | ϵ 
+            if (Actual_LookAhead == "+")
+            {
+                MatchToken("+");
+                if (F())
+                {
+                    return E_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == "-")
+            {
+                MatchToken("-");
+                if (F())
+                {
+                    return E_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
         }
         private bool F()
         {
+            if (G())
+            {
+                return F_Prime();
+            }
+            else
+            {
+                return false;
+
+            }
             return false;
         }
         private bool F_Prime()
         {
-            return false;
+            //F'-> *G F' | /G F' | %G F' | ϵ  
+            if (Actual_LookAhead == "*")
+            {
+                MatchToken("*");
+                if (G())
+                {
+                    return F_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == "/")
+            {
+                MatchToken("/");
+                if (G())
+                {
+                    return F_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == "%")
+            {
+                MatchToken("%");
+                if (G())
+                {
+                    return F_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return true;
+            }
         }
         private bool G()
         {
-            return false;
+            if (Actual_LookAhead == "!")
+            {
+                MatchToken("!");
+                return Expr();
+            }
+            else if (Actual_LookAhead == "-")
+            {
+                MatchToken("-");
+                return Expr();
+            }
+            else
+            {
+                return H();
+            }
         }
         private bool H()
         {
-            return false;
+            if (I())
+            {
+                return H_Prime();
+            }
+            else
+            {
+                return false;
+
+            }
         }
         private bool H_Prime()
         {
-            return false;
+            //H'-> [Expr] Igual' H' | . ident Igual' H' | ϵ
+            if (Actual_LookAhead == "[")
+            {
+                MatchToken("[");
+                if (Igual_Prime())
+                {
+                    return H_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (Actual_LookAhead == ".")
+            {
+                MatchToken(".");
+                MatchToken("ident");
+                if (Igual_Prime())
+                {
+                    return H_Prime();
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return H();
+            }
         }
         private bool I()
         {
-            return false;
+            if (Actual_LookAhead == "(")
+            {
+                if (Expr())
+                {
+                    MatchToken(")");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (Actual_LookAhead == "New")
+            {
+                MatchToken("New");
+                MatchToken("(");
+                MatchToken("ident");
+                MatchToken(")");
+                return true;
+            }
+            else if (Constant())
+            {
+                return Constant();
+            }
+            else if (Actual_LookAhead == "ident")
+            {
+                MatchToken("ident");
+                if (Igual_Prime())
+                {
+                    return Igual_Prime();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (Actual_LookAhead == "this")
+            {
+                MatchToken("this");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         private bool Igual_Prime()
         {
-            return false;
+            if (Actual_LookAhead == "=")
+            {
+                return Expr();
+            }
+            else
+            {
+                return true;
+            }
+
         }
         #endregion
         public void pushIntoList(string type, string dato)
