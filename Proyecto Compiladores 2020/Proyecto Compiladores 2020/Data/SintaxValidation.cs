0a17fcd0bfba3a596433f520037be2b94048aac8
MarlonRoches <roches856@gmail.com>
2020-10-14 20:10:05 -0600

    Instrucciones de la progra

--- Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/SintaxValidation.cs
+++ Proyecto Compiladores 2020/Proyecto Compiladores 2020/Data/SintaxValidation.cs
@@ -8,5 +8,23 @@ namespace Proyecto_Compiladores_2020.Data
 {
     class SintaxValidation
     {
+        //programar cada estado de la tabla
+        // obtener la pila 
+        // calcular lookahead
+        // 
+        // obtener lista de tokens
+        // error: leer hasta ; y seguir
+        // des stackear estados
+
+
+        //void Estado( stack de estados, cadena ,string actual ,string lookAhead)
+        //{
+        //    switch (actual)
+        //    {
+        //      case "ident" :
+        //   s18(stack de estados, cadena ,string actual ,string lookAhead):
+        //        default:
+        //    }
+        //}
     }
 }
